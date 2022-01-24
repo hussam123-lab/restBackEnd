2af2623e5af9c0c7813daf4c7bdbe8a1b404b5bf
hussam123-lab <62464644+hussam123-lab@users.noreply.github.com>
2022-01-24 12:44:19 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 ls = []
 @app.route('/', methods = ["POST","GET"])
 
-@app.route('/main')
+@app.route('/main',methods = ["POST","GET"])
 def getMain():
     ls.append(1)
     return (str(len(ls)))
