d55cf1fef91f22718eddcfe58f2b1272b366aabb
hussam123-lab <62464644+hussam123-lab@users.noreply.github.com>
2022-02-02 00:55:11 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,114 @@ import opsgenie_sdk
 app = Flask(__name__)
 opsgenie_api_key = '8fedb235-e7c0-4e2a-907f-05ef3dff1d01' #or your teams opsgenie key
 
+
+count = 0
+
+
 ls = []
 container_logs = {
-    "am-usage" : {},
-    "insights-logs-appservicehttplogs" : {},
-    "insights-metrics-pt1m": {}
-}
+    "am-usage":{
+    },
+    "insights-logs-appservicehttplogs":[
+     {
+       "metricName":"CsBytes",
+       "anomalies":{
+        "anomalies":[
+          
+        ],
+        "anomaly_labels":[
+          
+        ],
+        "anomaly_indexes":[
+          
+        ]
+       }
+     },
+     {
+       "metricName":"ScStatus",
+       "anomalies":{
+        "anomalies":[
+        ],
+        "anomaly_labels":[
+        ],
+        "anomaly_indexes":[
+        ]
+       }
+     },
+     {
+       "metricName":"TimeTaken",
+       "anomalies":{
+        "anomalies":[
+          
+        ],
+        "anomaly_labels":[
+         
+        ],
+        "anomaly_indexes":[
+          
+        ]
+       }
+     }
+    ],
+    "insights-metrics-pt1m":[
+     {
+       "metricName":"CpuTime",
+       "anomalies":{
+        "anomalies":[
+        ],
+        "anomaly_labels":[
+        ],
+        "anomaly_indexes":[
+        ]
+       }
+     },
+     {
+       "metricName":"BytesSent",
+       "anomalies":{
+        "anomalies":[
+        ],
+        "anomaly_labels":[
+        ],
+        "anomaly_indexes":[
+        ]
+       }
+     },
+     {
+       "metricName":"HttpResponseTime",
+       "anomalies":{
+        "anomalies":[
+        ],
+        "anomaly_labels":[
+        ],
+        "anomaly_indexes":[
+        ]
+       }
+     }
+    ]
+  }
+
+# {
+#     "am-usage" : {},
+#     "insights-logs-appservicehttplogs" : {},
+#     "insights-metrics-pt1m": {}
+# }
+
+# count_of_accesses = {
+
+# }
+
+# for i in container_logs["insights-logs-appservicehttplogs"]:
+#     count_of_accesses[i["metricName"]] = {
+#         "count" : 0,
+#         "latest_index" : 0
+#     }
+    
+# for i in container_logs["insights-metrics-pt1m"]:
+#     count_of_accesses[i["metricName"]] = {
+#         "count" : 0,
+#         "latest_index" : 0
+#     }
+# print(count_of_accesses)
 
 
 """
@@ -51,6 +153,10 @@ class opsgenieConfig:
         except opsgenie_sdk.ApiException as err:
             print("Exception when calling AlertApi->create_alert: %s\n" % err)
 
+
+checked = False
+
+
 @app.route('/', methods = ["POST","GET"])
 
 @app.route('/main',methods = ["POST","GET"])
@@ -61,19 +167,102 @@ def getMain():
         x = request.data.decode("utf-8") # gets data then goes from bytes to string
         x = json.loads(x)
         container_logs[x["name"]] = x["data"]
-        anomalies_csbytes = x["insights-logs-appservicehttplogs"][0]["anomalies"]["anomalies"] #Csbytes
-        anomalies_Scbytes = x["insights-logs-appservicehttplogs"][1]["anomalies"]["anomalies"] #ScStatus
-        anomalies_timeTaken = x["insights-logs-appservicehttplogs"][2]["anomalies"]["anomalies"] #TimeTaken
+
+        anomalies_csbytes = None
+        anomalies_Scbytes = None
+        anomalies_timeTaken = None
+
+        anomalies_CpuTime = None
+        anomalies_BytesSent = None
+        anomalies_HttpResponseTime = None
+
+        if x["name"] == "insights-logs-appservicehttplogs":
 
 
-        if anomalies_csbytes:
-            createAlert("insights-logs-appservicehttplogs", "CsBytes", -1)
+            anomalies_csbytes = x["data"][0]["anomalies"]["anomalies"] #Csbytes
+
         
-        if anomalies_Scbytes:
-            createAlert("insights-logs-appservicehttplogs", "ScBytes", -1)
+            anomalies_Scbytes = x["data"][1]["anomalies"]["anomalies"] #ScStatus
+ 
 
-        if anomalies_timeTaken:
-            createAlert("insights-logs-appservicehttplogs", "Time Taken", -1)
+            anomalies_timeTaken = x["data"][2]["anomalies"]["anomalies"] #TimeTaken
+
+
+            if len(anomalies_csbytes) > 0 :
+                createAlert("insights-logs-appservicehttplogs", "CsBytes", -1)
+                
+        
+            if len(anomalies_Scbytes) > 0:
+                createAlert("insights-logs-appservicehttplogs", "ScBytes", -1)
+                
+
+            if len(anomalies_timeTaken)>0:
+                createAlert("insights-logs-appservicehttplogs", "Time Taken", -1)
+                
+        if x["name"] == "insights-metrics-pt1m":
+            anomalies_CpuTime = x["data"][0]["anomalies"]["anomalies"] # CpuTime
+    
+            anomalies_BytesSent = x["data"][1]["anomalies"]["anomalies"] # Bytes Sent
+    
+            anomalies_HttpResponseTime = x["data"][2]["anomalies"]["anomalies"] # HTTP Response Time
+
+            if len(anomalies_CpuTime) > 0 :
+                createAlert("insights-metrics-pt1m", "CpuTime", -1)
+                
+        
+            if len(anomalies_BytesSent) > 0:
+                createAlert("insights-metrics-pt1m", "BytesSent", -1)
+                
+
+            if len(anomalies_HttpResponseTime)>0:
+                createAlert("insights-metrics-pt1m", "HttpResponseTime", -1)
+                
+        # for i in range(0,len(container_logs["insights-logs-appservicehttplogs"])):
+        #     curr_len = len(container_logs["insights-logs-appservicehttplogs"][i]["anomalies"]["anomalies"])
+        #     curr_count = count_of_accesses[container_logs["insights-logs-appservicehttplogs"][i]["metricName"]["count"]]
+        #     curr_indx = count_of_accesses[container_logs["insights-logs-appservicehttplogs"][i]["metricName"]["latest_index"]]
+
+        #     if curr_indx != len(container_logs["insights-logs-appservicehttplogs"][0]["anomalies"]["anomalies"]):
+        #         temp_count = len(container_logs["insights-logs-appservicehttplogs"][0]["anomalies"]["anomalies"])
+        #         if temp_count > curr_count:
+        #             if temp_count > 5:
+        #                 count_of_accesses[container_logs["insights-logs-appservicehttplogs"][i]["metricName"]["count"]] = 0
+        #                 print('send email')
+        #             else:
+        #                 count_of_accesses[container_logs["insights-logs-appservicehttplogs"][i]["metricName"]["count"]] = temp_count
+        #             count_of_accesses[container_logs["insights-logs-appservicehttplogs"][i]["metricName"]["latest_index"]] = curr_len - 1
+
+
+
+        # for i in range(0,len(container_logs["insights-metrics-pt1m"])):
+        #     curr_len = len(container_logs["insights-metrics-pt1m"][i]["anomalies"]["anomalies"])
+        #     curr_count = count_of_accesses[container_logs["insights-metrics-pt1m"][i]["metricName"]["count"]]
+        #     curr_indx = count_of_accesses[container_logs["insights-metrics-pt1m"][i]["metricName"]["latest_index"]]
+
+        #     if curr_indx != len(container_logs["insights-metrics-pt1m"][0]["anomalies"]["anomalies"]):
+        #         # temp_count = len(x["insights-metrics-pt1m"][0]["anomalies"]["anomalies"])
+ 
+
+        #         if temp_count > curr_count:
+        #             if temp_count > 5:
+        #                 count_of_accesses[container_logs["insights-metrics-pt1m"][i]["metricName"]["count"]] = 0
+        #                 print('send email')
+        #             else:
+        #                 count_of_accesses[container_logs["insights-metrics-pt1m"][i]["metricName"]["count"]] = temp_count
+        #             count_of_accesses[container_logs["insights-metrics-pt1m"][i]["metricName"]["latest_index"]] = curr_len - 1
+        
+                
+
+        # First Scenario repeat Scenarios 
+        
+
+
+
+        
+        
+
+
+
 
         
     return ('Rest API')
@@ -81,3 +270,7 @@ def getMain():
 def createAlert(description, meteric, value):
     the_genie = opsgenieConfig(opsgenie_api_key)
     the_genie.create(description, meteric, value)
+
+
+
+
