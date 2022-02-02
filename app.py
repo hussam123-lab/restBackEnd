f76f30d674cda0ab722e69e7e5138b07996ecc1a
hussam123-lab <62464644+hussam123-lab@users.noreply.github.com>
2022-02-02 23:44:49 +1100

    Update app.py

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ def createAlert(description, meteric, value, priority):
     the_genie = opsgenieConfig(opsgenie_api_key)
     the_genie.create(description, meteric, value, priority)
 
-app.run(host = "0.0.0.0",port = 5000)
+app.run()
 
 
 
